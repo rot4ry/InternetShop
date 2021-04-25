bce3f6dda82f19e390088596f0d5242ecde5369f
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:35:34 +0200

    Update: Entities/Product.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/Product.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/Product.cs
@@ -8,5 +8,11 @@ namespace DatabaseBuilder.Entities
 {
     public class Product
     {
+        public int ProductID { get; set; }
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProducerCode { get; set; }
     }
 }
