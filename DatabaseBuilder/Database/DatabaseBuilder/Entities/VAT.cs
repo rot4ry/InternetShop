67477554c414dff0d10e594f6c94ca97e900f851
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:57:43 +0200

    Update: Entities/VAT.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/VAT.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/VAT.cs
@@ -8,5 +8,8 @@ namespace DatabaseBuilder.Entities
 {
     public class VAT
     {
+        public DateTime DateSince { get; set; } //Primary Key
+        public DateTime? DateChanged { get; set; }
+        public decimal Value { get; set; }
     }
 }
