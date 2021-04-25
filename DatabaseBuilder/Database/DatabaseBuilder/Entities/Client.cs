71e6f9a0ef0b3409be03a2ed522c947c8d157f13
rot4ry <wojtekw233@gmail.com>
2021-04-25 21:46:07 +0200

    Update: Entities/Client.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/Client.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/Client.cs
@@ -8,5 +8,17 @@ namespace DatabaseBuilder.Entities
 {
     public class Client
     {
+        public int ClientID { get; set; }   //Primary Key
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public int? FlatNumber { get; set; }
+        public bool IsConstClient { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
     }
 }
