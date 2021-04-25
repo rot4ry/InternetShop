4f674884b7442ae26b1dde4d002ef3c9776e460e
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:46:30 +0200

    Update: Entities/ProductPicture.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/ProductPicture.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/ProductPicture.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Drawing;
 
 namespace DatabaseBuilder.Entities
 {
     public class ProductPicture
     {
+        public int PictureID { get; set; }
+        public int ProductID { get; set; }
+        //public Image Picture { get; set; }
     }
 }
