1aecd57bb8bef61bf144cd5eb75044e1e97271e4
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:49:36 +0200

    Update: Entities/ProductParameter.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/ProductParameter.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/ProductParameter.cs
@@ -8,5 +8,10 @@ namespace DatabaseBuilder.Entities
 {
     public class ProductParameter
     {
+        public int ParameterID { get; set; }    //Primary && Foreign Key
+        public int ProductID { get; set; }  //Primary && Foreign Key
+        public string ParameterValueText { get; set; }  //Not nullable!
+        public int? ParameterValueInt { get; set; }
+        public decimal? ParameterDecimal { get; set; }   
     }
 }
