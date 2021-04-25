58eb14883f53a2ee6425568c501a16700c4df2c4
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:28:34 +0200

    Update: Entities/Order.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/Order.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/Order.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Drawing.Bitmap;
 
 namespace DatabaseBuilder.Entities
 {
     public class Order
     {
+        public int OrderID { get; set; }    //Primary Key
+        public int ClientID { get; set; }   //Foreign Key
+        public DateTime ReceivedDate { get; set; }
+        public DateTime? PreparedDate { get; set; }
+        public DateTime? SentDate { get; set; }
+        public string SentToAddress { get; set; }
+        public bool IsInvoiced { get; set; }
+        //public Image? Invoice { get; set; }
+        //public Image? InvoiceCopy { get; set; }
     }
 }
