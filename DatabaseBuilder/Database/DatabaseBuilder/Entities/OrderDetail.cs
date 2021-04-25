fd7d91addc620513efd2c3ea8867e576f7ef6896
rot4ry <wojtekw233@gmail.com>
2021-04-25 22:32:41 +0200

    Update: Entities/OrderDetail.cs

--- DatabaseBuilder/Database/DatabaseBuilder/Entities/OrderDetail.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Entities/OrderDetail.cs
@@ -8,5 +8,9 @@ namespace DatabaseBuilder.Entities
 {
     public class OrderDetail
     {
+        public int OrderID { get; set; }    //Primare && Foreign Key
+        public int ProductID { get; set; }  //Primare && Foreign Key
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
     }
 }
