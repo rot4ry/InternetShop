77ae295b414848fc3bc666231068bc9aca225e88
rot4ry <wojtekw233@gmail.com>
2021-05-03 13:44:03 +0200

    Added: ContentBuilder/FixedCreator/ classes - describing the content of fixed tables in the DB.

---
+++ DatabaseBuilder/Database/DatabaseBuilder/ContentBuilder/FixedCreator/CategoryCreator.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseBuilder.FixedCreator
+{
+    /// <summary>
+    /// Builds a fixed table of type Category
+    /// </summary>
+    public class CategoryCreator
+    {
+    }
+}
---
+++ DatabaseBuilder/Database/DatabaseBuilder/ContentBuilder/FixedCreator/ParameterCreator.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseBuilder.FixedCreator
+{
+    /// <summary>
+    /// Builds a fixed table of type Parameter
+    /// </summary>
+    public class ParameterCreator
+    {
+    }
+}
