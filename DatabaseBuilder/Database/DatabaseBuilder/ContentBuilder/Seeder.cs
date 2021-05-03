92e09b647def38643d011ba65d2f9bae53b3d510
rot4ry <wojtekw233@gmail.com>
2021-05-03 13:44:38 +0200

    Added: Seeder.cs which will take care of filling the DB with data.

---
+++ DatabaseBuilder/Database/DatabaseBuilder/ContentBuilder/Seeder.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseBuilder.FixedCreator;
+using DatabaseBuilder.RandomCreator;
+
+namespace DatabaseBuilder.ContentBuilder
+{
+    /// <summary>
+    /// Uses ClientCreator, ProductCreator, CategoryBuilder and ParameterBuilder
+    /// to fill the database with random OR fixed (required) values.
+    /// </summary>
+    public class Seeder
+    {
+        //Fill Parameters
+        //Fill Categories
+        //Fill Products
+        //Fill Clients
+    }
+}
