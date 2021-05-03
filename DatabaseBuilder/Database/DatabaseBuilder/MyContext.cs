602e45ae34c846c411590296c04ce91f0af0e11e
rot4ry <wojtekw233@gmail.com>
2021-05-03 13:40:09 +0200

    Renaming Context.cs to MyContext.cs to make it clear.

---
+++ DatabaseBuilder/Database/DatabaseBuilder/MyContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatabaseBuilder.Entities;
+
+namespace DatabaseBuilder
+{
+    /// <summary>
+    /// Connects to the SQL Server based on the connection string
+    /// given in the constructor
+    /// Creates DB model
+    /// </summary>
+    public class MyContext : DbContext
+    {
+        public string _CONNECTIONSTRING { get; private set; }
+        public MyContext(string _cs)
+        {
+            _CONNECTIONSTRING = _cs;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_CONNECTIONSTRING);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
