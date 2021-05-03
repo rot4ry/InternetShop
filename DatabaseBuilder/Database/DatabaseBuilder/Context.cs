37803b22ae3585eae4ad4e1670453bac464a6918
rot4ry <wojtekw233@gmail.com>
2021-05-03 12:59:58 +0200

    Update: Context.OnConfiguring() method finished, adding a constructor for the class.

--- DatabaseBuilder/Database/DatabaseBuilder/Context.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/Context.cs
@@ -4,9 +4,19 @@ namespace DatabaseBuilder
 {
     public class Context : DbContext
     {
+        public string _CONNECTIONSTRING { get; private set; }
+        public Context(string _cs)
+        {
+            _CONNECTIONSTRING = _cs;
+            System.Console.WriteLine(_CONNECTIONSTRING);
+        }
+
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_CONNECTIONSTRING);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
