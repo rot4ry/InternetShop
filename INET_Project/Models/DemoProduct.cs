4b5a423408792d19202eb4d220b65795c4052e7f
Kamil <kp054467@student.ath.edu.pl>
2021-05-07 13:31:51 +0200

    Update: DemoProduct Models

--- INET_Project/Models/DemoProduct.cs
+++ INET_Project/Models/DemoProduct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,17 @@ namespace INET_Project.Models
 {
     public class DemoProduct
     {
+        [Required]
+        [Display(Name = "Nazwa produktu")]
         public string ProductName { get; set; }
+        [Required]
+        [Display(Name = "Opis produktu")]
         public string Description { get; set; }
+        [Required]
+        [Display(Name = "Zdjęcie produktu")]
         public string ProductImage { get; set; }
+        [Required]
+        [Display(Name = "Cena produktu")]
         public decimal Price { get; set; }
     }
 }
