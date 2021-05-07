be33d19c519338c64ff18fada2e784e8c2726e7f
Kamil <kp054467@student.ath.edu.pl>
2021-05-07 13:09:11 +0200

    Update: AdminController

--- INET_Project/Controllers/AdminController.cs
+++ INET_Project/Controllers/AdminController.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using INET_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace INET_Project.Controllers
 {
     public class AdminController : Controller
     {
+        private static List<DemoProduct> _demoProducts = new List<DemoProduct>();
         public IActionResult Dashboard()
         {
+            ViewBag.Products = _demoProducts;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Dashboard(DemoProduct products)
+        {
+            if (ModelState.IsValid)
+            {
+                _demoProducts.Add(products);
+                return RedirectToAction(nameof(Dashboard));
+            }
             return View();
         }
     }
