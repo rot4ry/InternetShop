aaf41e24425f6d95365b9a41ee3ac14fe88cf781
rot4ry <wojtekw233@gmail.com>
2021-05-09 19:29:25 +0200

    Update: ParameterCreator.cs

--- DatabaseBuilder/Database/DatabaseBuilder/ContentBuilder/FixedCreator/ParameterCreator.cs
+++ DatabaseBuilder/Database/DatabaseBuilder/ContentBuilder/FixedCreator/ParameterCreator.cs
@@ -1,9 +1,26 @@
-﻿namespace DatabaseBuilder.FixedCreator
+﻿using DatabaseBuilder.Entities;
+using System.Collections.Generic;
+
+namespace DatabaseBuilder.FixedCreator
 {
     /// <summary>
     /// Builds a fixed table of type Parameter
     /// </summary>
     public class ParameterCreator
     {
+        //public int ParameterID { get; set; }    //Primary Key  
+        //public string ParameterName { get; set; }
+        //public string? ParameterDescription { get; set; }
+
+        //create parameters set, add it to the database
+
+        //List<Parameter> parametersList = new List<Parameter>()
+        //{
+        //    new Parameter { ParameterName = "RAM Memory", ParameterDescription = ""},
+        //    new Parameter{},
+        //    new Parameter()
+        //};
+
+
     }
 }
