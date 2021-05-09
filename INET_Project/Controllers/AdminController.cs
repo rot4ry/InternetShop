261634e61ae8adce94c087d9774d9d0248da4049
Kamil <kp054467@student.ath.edu.pl>
2021-05-09 20:57:35 +0200

    Update: Dashboard method in AdminController

--- INET_Project/Controllers/AdminController.cs
+++ INET_Project/Controllers/AdminController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using INET_Project.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace INET_Project.Controllers
 {
     public class AdminController : Controller
     {
+        private IWebHostEnvironment _environment;
         private static List<DemoProduct> _demoProducts = new List<DemoProduct>();
+        public AdminController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
         [HttpGet]
         public IActionResult Dashboard()
         {
@@ -21,6 +29,12 @@ namespace INET_Project.Controllers
         {
             if (ModelState.IsValid)
             {
+                var imageName = HttpContext.Session.GetString("ImageName");
+                if (!string.IsNullOrEmpty(imageName))
+                {
+                    products.ProductImage = imageName;
+                    HttpContext.Session.SetString("ImageName", "");
+                }
                 _demoProducts.Add(products);
                 return RedirectToAction(nameof(Dashboard));
             }
@@ -41,6 +55,22 @@ namespace INET_Project.Controllers
         public IActionResult Categories()
         {
             return View();
+        }
+        public async Task ProductImage(IFormFile image)
+        {
+            if (image != null)
+            {
+                var name = $"{Guid.NewGuid()}_{image.FileName}";
+                HttpContext.Session.SetString("ImageName", name);
+
+                var toFolder = Path.Combine(_environment.WebRootPath, "images");
+                var filePath = Path.Combine(toFolder, name);
+
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+                await image.CopyToAsync(fileStream);
+            }
+            HttpContext.Session.SetString("ImageName", "");
+            return;
         }
     }
 }
