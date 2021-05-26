a9d6f6e3e5dd2da0ae586a6342a5ac909da273ae
Łukasz Pindel <80412416+LukaszPindel@users.noreply.github.com>
2021-05-26 16:15:43 +0200

    Added: ReturnDetail model

---
+++ INET_Project/Models/ReturnDetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INET_Project.Models
+{
+
+    public enum Type
+    {
+        [Display(Name = "Zwrot")]
+        Return,
+        [Display(Name = "Reklamacja")]
+        Replacement,
+    }
+
+    public class ReturnDetail
+    {
+        [Display(Name = "ID zamówienia")]
+        public int OrderId { get; set; }
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+        [Display(Name = "Data zakupu")]
+        public DateTime DateOfPurchase { get; set; }
+        [Display(Name = "Data otrzymania zamówienia")]
+        public DateTime DateOfReceiving { get; set; }
+        [Display(Name = "Nazwa produktu")]
+        public string ProductName { get; set; }
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+    }
+}
