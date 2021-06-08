447fa219f251c6591e9aec67efd5ee9ea95a9163
Łukasz Pindel <80412416+LukaszPindel@users.noreply.github.com>
2021-06-09 00:06:51 +0200

    Update: Product picture display

--- INET_Project/Models/Entities/ProductPicture.cs
+++ INET_Project/Models/Entities/ProductPicture.cs
@@ -1,10 +1,13 @@
-﻿namespace INET_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace INET_Project.Models
 {
     /// <summary>
     /// Describes database table
     /// </summary>
     public class ProductPicture
     {
+        [Key]
         public int PictureID { get; set; }  //Primary Key
         public int ProductID { get; set; }  //ForeignKey
         public string PicturePath { get; set; }
